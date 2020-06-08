08923f1a925f088c8535457f67730465a39416c5
ashton_morgan <luckyguy73@yahoo.com>
2020-06-07 21:24:16 -0700

    finished tic-tac-toe stage 5

--- Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Game.java
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Game.java
@@ -5,39 +5,44 @@ import java.util.stream.Stream;
 
 public class Game {
 
+    public enum State { TAKE_TURN, END }
+
     private final String[][] board = new String[3][];
     private final HashSet<String> set = new HashSet<>();
+    private State state;
+    private String c;
     private String field;
-    private String state;
+    private String status;
     private boolean hasEmptyCells;
     private boolean oWins;
     private boolean xWins;
-    private String c;
     private int oCount;
     private int xCount;
+    private int turnCount;
 
-    public Game(String s) {
+    public Game() {
+        this.field = "_".repeat(9);
         initializeBoard();
+        getInput();
     }
+
+    public void execute(String input) { if (state == State.TAKE_TURN) updateBoard(input); }
+
+    private void getInput() { System.out.print("Enter the coordinates: "); }
 
     private void initializeBoard() {
         for (int r = 0, c = 0; c < field.length(); ++r, c += 3)
             board[r] = new String[]{String.valueOf(field.charAt(c)), String.valueOf(field.charAt(c + 1)),
                     String.valueOf(field.charAt(c + 2))};
-    }
-
-    public void go() {
+        state = State.TAKE_TURN;
         printBoard();
-        checkBoard();
-        setState();
-        printState();
-        updateBoard();
     }
 
     private void printBoard() {
         System.out.println("-".repeat(9));
-        for (int r = 0; r < field.length(); r += 3)
-            System.out.printf("| %s %s %s |\n", field.charAt(r), field.charAt(r + 1), field.charAt(r + 2));
+        String temp = field.replace("_", " ");
+        for (int r = 0; r < temp.length(); r += 3)
+            System.out.printf("| %s %s %s |\n", temp.charAt(r), temp.charAt(r + 1), temp.charAt(r + 2));
         System.out.println("-".repeat(9));
     }
 
@@ -47,6 +52,7 @@ public class Game {
         checkCols();
         checkMainDiag();
         checkAltDiag();
+        setState();
     }
 
     private void countChars() {
@@ -93,43 +99,35 @@ public class Game {
 
     private void setState() {
         int diff = Math.abs(xCount - oCount);
-        if (xWins && oWins || diff > 1) state = "Impossible";
-        else if (!xWins && !oWins && !hasEmptyCells) state = "Draw";
-        else if (xWins) state = "X wins";
-        else if (oWins) state = "O wins";
-        else state = "Game not finished";
+        if (xWins && oWins || diff > 1) status = "Impossible";
+        else if (!xWins && !oWins && !hasEmptyCells) status = "Draw";
+        else if (xWins) status = "X wins";
+        else if (oWins) status = "O wins";
+        else status = "Game not finished";
+        if (!status.equals("Game not finished")) {
+            state = State.END;
+            printState();
+        }
     }
 
-    private void printState() { System.out.println(state); }
+    private void printState() { System.out.println(status); }
 
-    private void updateBoard() {
-        Scanner scan = new Scanner(System.in);
+    private void updateBoard(String input) {
         int num1, num2;
-        boolean loop = true;
-        while (loop) {
-            System.out.print("Enter the coordinates: ");
-            String input = scan.nextLine();
-            if (Character.isDigit(input.charAt(0)) && Character.isDigit(input.charAt(2))) {
-                num1 = Integer.parseInt(input.substring(0, 1));
-                num2 = Integer.parseInt(input.substring(2));
-                if (num1 > 0 && num1 < 4 & num2 > 0 & num2 < 4) {
-                    int[] nums = convertCoordinates(num1, num2);
-                    if (board[nums[0]][nums[1]].equals("_")) {
-                        board[nums[0]][nums[1]] = "X";
-                        loop = false;
-                    } else {
-                        System.out.println("This cell is occupied! Choose another one!");
-                    }
-                } else {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                }
-            } else {
-                System.out.println("You should enter numbers!");
-            }
-        }
-        scan.close();
-        field = String.join("", Stream.of(board).flatMap(Stream::of).toArray(String[]::new));
-        printBoard();
+        if (Character.isDigit(input.charAt(0)) && Character.isDigit(input.charAt(2))) {
+            num1 = Integer.parseInt(input.substring(0, 1));
+            num2 = Integer.parseInt(input.substring(2));
+            if (num1 > 0 && num1 < 4 & num2 > 0 & num2 < 4) {
+                int[] nums = convertCoordinates(num1, num2);
+                if (board[nums[0]][nums[1]].equals("_")) {
+                    board[nums[0]][nums[1]] = turnCount++ % 2 == 0 ? "X" : "O";
+                    field = String.join("", Stream.of(board).flatMap(Stream::of).toArray(String[]::new));
+                    printBoard();
+                    checkBoard();
+                } else System.out.println("This cell is occupied! Choose another one!");
+            } else System.out.println("Coordinates should be from 1 to 3!");
+        } else System.out.println("You should enter numbers!");
+        if (state != State.END) getInput();
     }
 
     private int[] convertCoordinates(int first, int second) {
@@ -137,5 +135,7 @@ public class Game {
         int col = first - 1;
         return new int[]{row, col};
     }
+
+    public State getState() { return this.state; }
 
 }
